7a8226f96ebe9f7b9aa3379eeda8019f3d10e308
Datta973 <mvdatta3@gmail.com>
2017-12-14 17:37:01 +0530

    src Change 1

--- src/js/worker.js
+++ src/js/worker.js
@@ -3,5 +3,6 @@ $(document).ready(()=>{
     $("#title").addClass('col-md-2 col-md-offset-5',250,function(){
         $("#stats").show(250);
     });
+    console.log($("#stats").children().addClass('hbar'));
     
 })
