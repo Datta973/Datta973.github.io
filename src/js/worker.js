14bdf8a3e8aac3f4820d3a0460c1d89ca46536fa
Datta973 <mvdatta3@gmail.com>
2017-12-25 17:45:51 +0530

    Added src

--- src/js/worker.js
+++ src/js/worker.js
@@ -3,7 +3,5 @@ $(document).ready(()=>{
     $("#title").addClass('col-md-2 col-md-offset-5',250,function(){
         $("#stats").show(500);
     });
-    $("#stats").children().each(function(){
-        $(this).addClass('hbar');
-    })
+    
 })
