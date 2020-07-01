2e1068b7ff1c7734b70312a0c87211e7bdfcc56c
Datta973 <mvdatta3@gmail.com>
2020-07-01 17:34:58 +0530

    Made a lot of changes

--- src/js/worker.js
+++ src/js/worker.js
@@ -1,7 +1,47 @@
-$("#stats").hide();
-$(document).ready(()=>{
-    $("#title").addClass('col-md-2 col-md-offset-5',250,function(){
-        $("#stats").show(500);
+$(document).ready(() => {
+  // $("#title").addClass('col-md-2 col-md-offset-5',250,function(){
+  //     $("#stats").show(500);
+  // });
+
+  let focused = false;
+
+  $(".picture").addClass("expand");
+
+  $("#icon-education").click(e => {
+    e.stopPropagation();
+    focused = true;
+
+    $("#panel-item-skills").hide();
+    $("#panel-item-projects").hide();
+    $("#panel-item-education").show(() => {
+      $("#details-wrapper").show(250);
     });
-    
-})
+  });
+
+  $("#icon-skills").click(e => {
+    e.stopPropagation();
+    focused = true;
+    $("#panel-item-education").hide();
+    $("#panel-item-projects").hide();
+    $("#panel-item-skills").show(() => {
+      $("#details-wrapper").show(250);
+    });
+  });
+
+  $("#icon-projects").click(e => {
+    e.stopPropagation();
+    focused = true;
+    $("#panel-item-education").hide();
+    $("#panel-item-skills").hide();
+    $("#panel-item-projects").show(() => {
+      $("#details-wrapper").show(250);
+    });
+  });
+
+  $(document).click(() => {
+    focused = !focused;
+    if (!focused) {
+      $("#details-wrapper").hide(250);
+    }
+  });
+});
